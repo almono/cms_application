3e64aac1c0cc0952e0769a007c2ad2c1762b84a0
almono <pwalczykiewicz93@gmail.com>
2019-03-31 19:24:26 +0100

    Editable about me sections in admin panel

--- app/Http/Controllers/Panel/AboutMeController.php
+++ app/Http/Controllers/Panel/AboutMeController.php
@@ -22,7 +22,7 @@ class AboutMeController extends Controller
      */
     public function index()
     {
-        $info = AboutMe::first();
+        $info = AboutMe::first(); //dd($info->main);
         return view('admin.cms_about_me', compact('info'));
     }
 
@@ -80,12 +80,52 @@ class AboutMeController extends Controller
     {
         $info = AboutMe::first();
         $params = $request->except('_token','_method');
-        //dd($params);
 
-        $updates = array_combine($params['main']['key'],$params['main']['value']);
 
-        $info->main = $updates;
-        $info->save();
+        foreach($params['main'] as $main) {
+            if(isset($main['keys']) && isset($main['values'])) {
+                $main_info[] = array_combine($main['keys'],$main['values']);
+            }
+        }
+
+        foreach($params['education'] as $education) {
+            if(isset($education['keys']) && isset($education['values'])) {
+                $education_info[] = array_combine($education['keys'],$education['values']);
+            }
+        }
+
+        foreach($params['experience'] as $experience) {
+            if(isset($experience['keys']) && isset($experience['values'])) {
+                $experience_info[] = array_combine($experience['keys'],$experience['values']);
+            }
+        }
+
+        foreach($params['skills'] as $skills) {
+            if(isset($skills['keys']) && isset($skills['values'])) {
+                $skills_info[] = array_combine($skills['keys'],$skills['values']);
+            }
+        }
+
+        foreach($params['interests'] as $interests) {
+            if(isset($interests['keys']) && isset($interests['values'])) {
+                $interests_info[] = array_combine($interests['keys'],$interests['values']);
+            }
+        }
+
+        $info->main = $main_info;
+        $info->education = $education_info;
+        $info->experience = $experience_info;
+        $info->skills = $skills_info;
+        $info->interests = $interests_info;
+
+        try {
+            $info->save();
+        } catch (\Exception $e) {
+            Log::info('Error while saving about me information! ' . $e->getMessage());
+            flash()->error("There has been an error while saving new about me information");
+            return back();
+        }
+
 
         return back();
 
--- resources/views/admin/cms_about_me.blade.php
+++ resources/views/admin/cms_about_me.blade.php
@@ -18,78 +18,157 @@
         </div>
 
     </div>
-    <div class="col-xs-12 col-sm-offset-3 col-sm-6 padding_fix">
-        <div class="col-xs-12 padding_fix cms-headings" style="height: 30px; margin-bottom: 5px">
-            <div class="col-xs-12 text-left" style="border-bottom: 1px solid black; color: white; line-height: 30px;">
-                <h4 style="font-weight: 600;">Main information</h4>
+    <div class="col-xs-12 col-sm-12 padding_fix">
+        <div class="col-xs-12 col-sm-6">
+            <div class="col-xs-12 padding_fix cms-headings" style="height: 30px; margin-bottom: 5px">
+                <div class="col-xs-12 text-left" style="border-bottom: 1px solid black; color: white; line-height: 30px;">
+                    <h4 style="font-weight: 600;">Main information</h4>
+                </div>
             </div>
-        </div>
-        <div class="col-xs-12" id="main_list" style="padding: 15px; border-bottom: 1px solid black; margin-bottom: 20px;">
-            @if(isset($info->main) && !is_null($info->main))
-                <?php $main_field = 1; ?>
-                @foreach($info->main as $main)
-                    <div class="col-xs-12 text-center main_field" id="main_inputs{{ $main_field }}">
-                        <h4 class="col-xs-12 text-center">Main field {{ $main_field }}</h4>
-                        <button type="button" class="btn btn-new" id="add_values_main" name="main_inputs{{ $main_field }}" value="main">Add new values</button>
-                        <i class="fa fa-times input-remover" id="field_remover"></i>
-                        @foreach($main as $key => $value)
-                            <div class="col-xs-12 padding_fix about-div text-center" id="main_{{ $main_field }}">
-                                <input class="dark-input" type="text" name="main[main_inputs{{$main_field}}][keys][]" value="{{ $key }}">
-                                <input class="dark-input" type="text" name="main[main_inputs{{$main_field}}][values][]" value="{{ $value }}">
-                                <i class="fa fa-times input-remover" id="input_remover"></i>
-                            </div>
-                        @endforeach
-                    </div>
-                    <?php $main_field += 1; ?>
-                @endforeach
-            @endif
-        </div>
-        <button type="button" class="btn btn-new" id="add_new_main" name="main" value="{{ $main_field }}" style="float: right; border: 2px solid black; margin-bottom: 1px; border-bottom: 0px; margin-top: 10px;">Add new field</button>
-
-        <div class="col-xs-12" id="education_list" style="padding: 15px; border-bottom: 1px solid black; margin-bottom: 20px;">
-            @if(isset($info->education) && !is_null($info->education))
-                <?php $education_field = 1; ?>
-                @foreach($info->education as $edu)
-                    <div class="col-xs-12 text-center education_field" id="education_inputs{{ $education_field }}">
-                        <h4 class="col-xs-12 text-center">Education field {{ $education_field }}</h4>
-                        <button type="button" class="btn btn-new" id="add_values_education" name="education_inputs{{ $education_field }}" value="education">Add new values</button>
-                        <i class="fa fa-times input-remover" id="field_remover"></i>
-                        @foreach($edu as $key => $value)
-                            <div class="col-xs-12 padding_fix about-div text-center" id="education_{{ $education_field }}">
-                                <input class="dark-input" type="text" name="education[education_inputs{{$education_field}}][keys][]" value="{{ $key }}">
-                                <input class="dark-input" type="text" name="education[education_inputs{{$education_field}}][values][]" value="{{ $value }}">
-                                <i class="fa fa-times input-remover" id="input_remover"></i>
-                            </div>
-                        @endforeach
-                    </div>
-                    <?php $education_field += 1; ?>
-                @endforeach
-            @endif
-        </div>
-        <button type="button" class="btn btn-new" id="add_new_education" name="education" value="{{ $education_field }}" style="float: right; border: 2px solid black; margin-bottom: 1px; border-bottom: 0px; margin-top: 10px;">Add new field</button>
-
-    <?php /*
-        <div class="col-xs-12 padding_fix cms-headings" style="height: 30px; margin-bottom: 5px">
-            <div class="col-xs-12 text-left" style="border-bottom: 1px solid black; color: white; line-height: 30px;">
-                <span>Education</span>
-                <button type="button" class="btn btn-new" id="add_new_education" name="education" style="float: right; border: 2px solid black; margin-bottom: 1px; border-bottom: 0px;">Add new</button>
+            <div class="col-xs-12" id="main_list" style="padding: 15px; margin-bottom: 20px;">
+                @if(isset($info->main) && !is_null($info->main))
+                    <?php $main_field = 1; ?>
+                    @foreach($info->main as $main)
+                        <div class="col-xs-12 text-center main_field" id="main_inputs{{ $main_field }}">
+                            <h4 class="col-xs-12 text-center">Main field {{ $main_field }}</h4>
+                            <button type="button" class="btn btn-new" id="add_values_main" name="main_inputs{{ $main_field }}" value="main">Add new values</button>
+                            <i class="fa fa-times input-remover" id="field_remover"></i>
+                            @foreach($main as $key => $value)
+                                <div class="col-xs-12 padding_fix about-div text-center" id="main_{{ $main_field }}">
+                                    <input class="dark-input" type="text" name="main[main_inputs{{$main_field}}][keys][]" value="{{ $key }}">
+                                    <input class="dark-input" type="text" name="main[main_inputs{{$main_field}}][values][]" value="{{ $value }}">
+                                    <i class="fa fa-times input-remover" id="input_remover"></i>
+                                </div>
+                            @endforeach
+                        </div>
+                        <?php $main_field += 1; ?>
+                    @endforeach
+                @endif
             </div>
+            <button type="button" class="btn btn-new" id="add_new_main" name="main" value="{{ $main_field }}" style="float: right; margin-top: 10px;">Add new field</button>
         </div>
-        <div class="col-xs-12" id="education_list" style="padding: 15px; border-bottom: 1px solid black;">
-            @if(isset($info->education) && !is_null($info->education))
-                @foreach($info->education as $key => $value)
-                    <div class="col-xs-12 padding_fix about-div text-center">
-                        <input class="dark-input" type="text" name="education[key][]" value="{{ $key }}">
-                        <input class="dark-input" type="text" name="education[value][]" value="{{ $value }}">
-                        <i class="fa fa-times input-remover" id="input_remover"></i>
-                    </div>
-                @endforeach
-            @endif
+
+        <div class="col-xs-12 col-sm-6">
+            <div class="col-xs-12 padding_fix cms-headings" style="height: 30px; margin-bottom: 5px">
+                <div class="col-xs-12 text-left" style="border-bottom: 1px solid black; color: white; line-height: 30px;">
+                    <h4 style="font-weight: 600;">Education information</h4>
+                </div>
+            </div>
+            <div class="col-xs-12" id="education_list" style="padding: 15px; margin-bottom: 20px;">
+                @if(isset($info->education) && !is_null($info->education))
+                    <?php $education_field = 1; ?>
+                    @foreach($info->education as $edu)
+                        <div class="col-xs-12 text-center education_field" id="education_inputs{{ $education_field }}">
+                            <h4 class="col-xs-12 text-center">Education field {{ $education_field }}</h4>
+                            <button type="button" class="btn btn-new" id="add_values_education" name="education_inputs{{ $education_field }}" value="education">Add new values</button>
+                            <i class="fa fa-times input-remover" id="field_remover"></i>
+                            @foreach($edu as $key => $value)
+                                <div class="col-xs-12 padding_fix about-div text-center" id="education_{{ $education_field }}">
+                                    <input class="dark-input" type="text" name="education[education_inputs{{$education_field}}][keys][]" value="{{ $key }}">
+                                    <input class="dark-input" type="text" name="education[education_inputs{{$education_field}}][values][]" value="{{ $value }}">
+                                    <i class="fa fa-times input-remover" id="input_remover"></i>
+                                </div>
+                            @endforeach
+                        </div>
+                        <?php $education_field += 1; ?>
+                    @endforeach
+                @endif
+            </div>
+            <button type="button" class="btn btn-new" id="add_new_education" name="education" value="{{ $education_field }}" style="float: right; margin-top: 10px;">Add new field</button>
         </div>
-*/ ?>
+
+        <div class="col-xs-12 col-sm-6">
+            <div class="col-xs-12 padding_fix cms-headings" style="height: 30px; margin-bottom: 5px">
+                <div class="col-xs-12 text-left" style="border-bottom: 1px solid black; color: white; line-height: 30px;">
+                    <h4 style="font-weight: 600;">Experience information</h4>
+                </div>
+            </div>
+            <div class="col-xs-12" id="experience_list" style="padding: 15px; margin-bottom: 20px;">
+                @if(isset($info->experience) && !is_null($info->experience))
+                    <?php $experience_field = 1; ?>
+                    @foreach($info->experience as $edu)
+                        <div class="col-xs-12 text-center experience_field" id="experience_inputs{{ $experience_field }}">
+                            <h4 class="col-xs-12 text-center">Experience field {{ $experience_field }}</h4>
+                            <button type="button" class="btn btn-new" id="add_values_experience" name="experience_inputs{{ $experience_field }}" value="experience">Add new values</button>
+                            <i class="fa fa-times input-remover" id="field_remover"></i>
+                            @foreach($edu as $key => $value)
+                                <div class="col-xs-12 padding_fix about-div text-center" id="experience_{{ $experience_field }}">
+                                    <input class="dark-input" type="text" name="experience[experience_inputs{{$experience_field}}][keys][]" value="{{ $key }}">
+                                    <input class="dark-input" type="text" name="experience[experience_inputs{{$experience_field}}][values][]" value="{{ $value }}">
+                                    <i class="fa fa-times input-remover" id="input_remover"></i>
+                                </div>
+                            @endforeach
+                        </div>
+                        <?php $experience_field += 1; ?>
+                    @endforeach
+                @endif
+            </div>
+            <button type="button" class="btn btn-new" id="add_new_experience" name="experience" value="{{ $experience_field }}" style="float: right;  margin-top: 10px;">Add new field</button>
+        </div>
+
+        <div class="col-xs-12 col-sm-6">
+            <div class="col-xs-12 padding_fix cms-headings" style="height: 30px; margin-bottom: 5px">
+                <div class="col-xs-12 text-left" style="border-bottom: 1px solid black; color: white; line-height: 30px;">
+                    <h4 style="font-weight: 600;">Skills information</h4>
+                </div>
+            </div>
+            <div class="col-xs-12" id="skills_list" style="padding: 15px; margin-bottom: 20px;">
+                @if(isset($info->skills) && !is_null($info->skills))
+                    <?php $skills_field = 1; ?>
+                    @foreach($info->skills as $edu)
+                        <div class="col-xs-12 text-center skills_field" id="skills_inputs{{ $skills_field }}">
+                            <h4 class="col-xs-12 text-center">Skills field {{ $skills_field }}</h4>
+                            <button type="button" class="btn btn-new" id="add_values_skills" name="skills_inputs{{ $skills_field }}" value="skills">Add new values</button>
+                            <i class="fa fa-times input-remover" id="field_remover"></i>
+                            @foreach($edu as $key => $value)
+                                <div class="col-xs-12 padding_fix about-div text-center" id="skills_{{ $skills_field }}">
+                                    <input class="dark-input" type="text" name="skills[skills_inputs{{$skills_field}}][keys][]" value="{{ $key }}">
+                                    <input class="dark-input" type="text" name="skills[skills_inputs{{$skills_field}}][values][]" value="{{ $value }}">
+                                    <i class="fa fa-times input-remover" id="input_remover"></i>
+                                </div>
+                            @endforeach
+                        </div>
+                        <?php $skills_field += 1; ?>
+                    @endforeach
+                @endif
+            </div>
+            <button type="button" class="btn btn-new" id="add_new_skills" name="skills" value="{{ $skills_field }}" style="float: right; margin-top: 10px;">Add new field</button>
+        </div>
+
+       <div class="col-xs-12 col-sm-6">
+           <div class="col-xs-12 padding_fix cms-headings" style="height: 30px; margin-bottom: 5px">
+               <div class="col-xs-12 text-left" style="border-bottom: 1px solid black; color: white; line-height: 30px;">
+                   <h4 style="font-weight: 600;">Interests information</h4>
+               </div>
+           </div>
+           <div class="col-xs-12" id="interests_list" style="padding: 15px; margin-bottom: 20px;">
+               @if(isset($info->interests) && !is_null($info->interests))
+                   <?php $interests_field = 1; ?>
+                   @foreach($info->interests as $edu)
+                       <div class="col-xs-12 text-center interests_field" id="interests_inputs{{ $interests_field }}">
+                           <h4 class="col-xs-12 text-center">Interests field {{ $interests_field }}</h4>
+                           <button type="button" class="btn btn-new" id="add_values_interests" name="interests_inputs{{ $interests_field }}" value="interests">Add new values</button>
+                           <i class="fa fa-times input-remover" id="field_remover"></i>
+                           @foreach($edu as $key => $value)
+                               <div class="col-xs-12 padding_fix about-div text-center" id="interests_{{ $interests_field }}">
+                                   <input class="dark-input" type="text" name="interests[interests_inputs{{$interests_field}}][keys][]" value="{{ $key }}">
+                                   <input class="dark-input" type="text" name="interests[interests_inputs{{$interests_field}}][values][]" value="{{ $value }}">
+                                   <i class="fa fa-times input-remover" id="input_remover"></i>
+                               </div>
+                           @endforeach
+                       </div>
+                       <?php $interests_field += 1; ?>
+                   @endforeach
+               @endif
+           </div>
+           <button type="button" class="btn btn-new" id="add_new_interests" name="interests" value="{{ $interests_field }}" style="float: right; margin-top: 10px;">Add new field</button>
+       </div>
+
+
         <div class="col-xs-12 text-center" style="margin-top: 15px;">
             <input type="submit" class="btn btn-new" value="Update">
         </div>
+
     </div>
     {!! Form::close() !!}
 @stop
